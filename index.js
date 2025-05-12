d2b74f980a78caad2350b9397fec1e067e0c5da2
Z125057-mael-minard <z125057@shibaura-it.ac.jp>
2025-05-12 18:02:21 +0900

    wow

---
+++ index.js
@@ -0,0 +1,16 @@
+//const video = document.getElementById('bg-video');
+//video.pause(); // Ensure video is paused initially
+//
+//const onScroll = () => {
+//  const distance = window.scrollY;
+//  const total = document.documentElement.scrollHeight;
+//
+//  let percentage = distance / total;
+//  percentage = Math.max(0, percentage);
+//  percentage = Math.min(percentage, 1);
+//
+//  video.currentTime = video.duration * percentage;
+//}
+//
+//onScroll();
+//window.addEventListener('scroll', onScroll);
